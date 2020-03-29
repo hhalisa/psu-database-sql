dc1926dba7dc57b4a4a81fd46482ca5150b55651
Haley Hartsaw <haleyhart123@gmail.com>
2020-03-29 16:20:14 -0500

    add schema

---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE sailor(
+	sid INTEGER,
+	sname VARCHAR(50),
+	rating INTEGER,
+	age NUMERIC(3, 1),
+	CONSTRAINT sailor_pk PRIMARY KEY(sid)
+);
+
+CREATE TABLE boat(
+	bid INTEGER,
+	bname VARCHAR(50),
+	color VARCHAR(50),
+	CONSTRAINT boat_pk PRIMARY KEY(bid)
+);
+
+CREATE TABLE reservation(
+	sid INTEGER,
+	bid INTEGER,
+	day DATE,
+	CONSTRAINT reservation_pk PRIMARY KEY(sid, bid),
+	CONSTRAINT reservation_has_sid_fk FOREIGN KEY (sid) REFERENCES sailor(sid) ON DELETE CASCADE,
+	CONSTRAINT reservation_has_bid_fk FOREIGN KEY (bid) REFERENCES boat(bid) ON DELETE CASCADE);
+
+
