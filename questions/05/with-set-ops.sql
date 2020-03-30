d6a8b4f3b34ed386d33568a60a4968839f7f4711
Haley Hartsaw <haleyhart123@gmail.com>
2020-03-30 12:37:56 -0500

    wrap question 5 queries in transaction

--- questions/05/with-set-ops.sql
+++ questions/05/with-set-ops.sql
@@ -1,3 +1,5 @@
+BEGIN TRANSACTION;
+
 SELECT DISTINCT s.sname
 FROM sailor s, reservation r, boat b
 WHERE s.sid = r.sid AND r.bid = b.bid AND b.color = 'red'
@@ -5,3 +7,5 @@ UNION
 SELECT DISTINCT s.sname
 FROM sailor s, reservation r, boat b
 WHERE s.sid = r.sid AND r.bid = b.bid AND b.color = 'green';
+
+ROLLBACK TRANSACTION;
--- questions/05/without-set-ops.sql
+++ questions/05/without-set-ops.sql
@@ -1,3 +1,7 @@
+BEGIN TRANSACTION;
+
 SELECT DISTINCT s.sname
 FROM sailor s, boat b, reservation r
 WHERE s.sid = r.sid AND r.bid = b.bid AND (b.color = 'red' OR b.color = 'green');
+
+ROLLBACK TRANSACTION;
